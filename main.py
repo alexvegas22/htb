c07d49e9474fc750b4c78d0b696d0b28d11a9f42
Alexandre Baudouin Vegas <abaudouinvegas@dept-info.crosemont.quebec>
2024-02-04 01:26:51 -0500

    switched to flask instead of nextjs

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+from markupsafe import escape
+
+url_for('static', filename='style.css')
+
+    
+@app.route("/")
+def hello_world():
+    return "<h1>HTB</h1>"
+
+@app.route("/chatroom")
+def chatroom_join():
+    return "<h1>HTB</h1>"
+
+
+@app.route("/chatroom/<room>")
+def chatroom():
+    return "<h1>HTB</h1>"
