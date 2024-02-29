c9a28ca03a58bd2ff406ab026750ff04f103d4a7
Alexandre Baudouin Vegas <abaudouinvegas@dept-info.crosemont.quebec>
2024-02-28 23:48:52 -0500

    added boards as options in profile section

--- htb-frontend/src/components/general.vue
+++ htb-frontend/src/components/general.vue
@@ -6,24 +6,33 @@ import Posts from './posts.vue';
 import Boards from './boards.vue';
 
 const posts = ref([]);
+const board = ref('general')
 
 const fetchData = async () => {
   try {
-    const response = await fetch('http://localhost:5000/general/posts');
+    const response = await fetch(`http://localhost:5000/${board.value}/posts`);
     posts.value = await response.json();
   } catch (error) {
     console.error('Error fetching posts:', error);
   }
 };
 
+function switchBoard(newBoard){
+    board.value = newBoard
+}
+
 watchEffect(() => {
   fetchData();
 });
 </script>
 
-<template>
+<template> 
+
+
   <div class="home-container">
-    <Profile />
+    <Profile :board="board" @switchboard="switchBoard"/>
+  
+    
     <div class="feed">
       <Posts v-for="item in posts" :key="item.id" :post="item" />
     </div>
